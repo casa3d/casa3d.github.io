84c5df05508d22252fc7cee1cbd18e5ee1725927
fernandoavila <luisfernandoavila@outlook.com>
2014-10-17 09:56:58 +0000

    en support (dv)
    english JavScript files for support
    (debugging version)

---
+++ support/js/en/full/faq.js
@@ -0,0 +1,9 @@
+//❔ frequent asked questions
+faq=['how to add window to house_press 1st option ➕ and look for it, (remember currently are 6 kind of textures)',
+  "how to resize the windo_press 2nd option ✋↔ then hover mouse over object's border (in this case window) and if resizing arrows appear then you're done, just drag it",
+  'how to add my own picture_just press 3rd option 🌄📂 and find out what happens',
+  "how to watch around_4th option ⌖ means position indicator, it enables rotation so, if you'd like to do it with your mouse then make sure that this option is enabled & then just move mouse, otherwise 5th ⟲ & 6th option ⟳ are useful since they rotate 90° to their own side",
+  "how to disable rotation_press 4th option ⌖ (position indicator), it enables or disables that",
+  'there is too much light so, how or when it gets darker_regarding to light intensity... 7th option ✳ decreases it whilst 8th ☀ does opposite',
+  "I'd like download this house and make it mine, can I_off course you can just press last but one option 📥 (download), take a look at terms & conditions though",
+  'is there any fullscreen option_ahah, last option 💢 goes with that']
---
+++ support/js/en/full/rqlist.js
@@ -0,0 +1,6 @@
+l='<br>'
+//☑ requirements list
+rq=['computer_may be laptop or desktop as long as it includes a good processor',
+'processor_it\'s compulsory a processor equal or superior to 1.4Ghz'+l+'because it\'ll be using it 60x per second',
+'any of following web-browsers_(recommended, not to worry it\'s based on Chromium) 搜狗高速浏览器 (sogou high-speed browser) since vr5'+l+'Mozilla Firefox since vr32'+l+'Microsoft IE since vr11'+l+'Google Chrome since vr30'+l+'Maxthon Browser since vr4.41'+l+'Яндекс.Браузер (Yandex.Browser) since vr14.5',
+'internet access_it\'s necessary that you at least for first time visit page with a fair speed internet connection']
