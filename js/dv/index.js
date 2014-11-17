84aee01b6064ed720bed1f87bc8132fd53f61f78
fernandoavila <luisfernandoavila@outlook.com>
2014-11-17 20:17:07 +0000

    upgrade

--- js/dv/index.js
+++ js/dv/index.js
@@ -1,90 +1,135 @@
 u={
-	K:' casa3d.hostzi.com',
-	lng:/es/.test(navigator.language)?'es':'en',
-	d:document,
-	p:chk.parentNode,
-	lp:lnk.parentNode,
-  c:function(tg){return u.d.createElement(tg)},
-  g:function(n){return u.d.getElementsByTagName(n)},
-	tx:function(e){var t=e.innerHTML.split(/[<>]/)//text
-		e.innerHTML='Estoy de acuerdo con estos <'+t[1]+'>Terminos & Condiciones de uso</a>'},
-	t:function(n){//test
-		return new RegExp(n?'\\w{4}':'[a-z]{3}').test((u.st=u.i[n||0].value))&&!/\W/.test(u.st)},
-	shift:function(){//i.c('called shift fn')
-		$('canvas').toggleClass('no')
-		h.R(u.a=!u.a)},
-	ss:function(v){//set session:value
-		i.Δ.user=v||0
-		i.tr('set','session')},
-	in:function(opt){
-		i.cmp=function(){
+  cl:'one two three'.split(' '),
+  d:document,
+  hch:hd.children,//head children
+  nv:navigator,
+  ich:intro.children,
+  p:chk.parentNode,
+  sp:lnk.parentNode,
+  c:function(tg){
+    return u.d.createElement(tg)},
+  g:function(n){
+    return u.d.getElementsByTagName(n)},
+  gc:function(e,cl,act){cl=cl||'active',act=
+    u.d.getElementsByClassName(cl)[0]
+    //console.log('prev active',act)
+    act&&(
+      //console.log('removing...'),
+      act.className='')
+    e.className=cl},
+  tx:function(e){var t=e.innerHTML.split(/[<>]/)//text
+    e.innerHTML='Estoy de acuerdo con estos <'+t[1]+'>Terminos & Condiciones de uso</a>'},
+  t:function(n){//test
+    return new RegExp(n?'\\w{4}':'[a-z]{3}').test((u.st=u.i[n||0].value))&&!/\W/.test(u.st)},
+  shcl:function(){//class
+    u.p.className=u.p.className=='no'?'':'no'
+    u.value.className=(u.lg=!u.lg)||u.match()},
+  shCl:function(i,lim){//shift class
+    if(roll.className!=(lim||u.cl[this.idx||i])){
+      //console.log('i:',i,'idx:',this.idx)
+      roll.className=u.cl[this.idx||i]
+      i=i>-1?
+        u.hch[i]
+        :this
+      //console.log('next:',i)
+      u.gc(i)}},
+  shift:function(){//i.c('called shift fn')
+    $('canvas').toggleClass('no')
+    h.R(u.a=!u.a)},
+  ss:function(v){//set session:value
+    i.Δ.user=v||0
+    i.tr('set','session')},
+  in:function(opt){
+    i.cmp=function(){
       u.a===!1&&(
-  			u.ss(u.i[0].value),
-  			//i.c('Logged In as: '+i.key),
-  			u.i[0].value=u.i[1].value=u.i[2].value='',
-  			u.shift())}
-		i.tr(opt)},
-	vl:function(){var n=i.key=u.i[0].value,v=i.val=u.i[1].value,v=Number(v)||v//validate
-		u.lg?
-			u.t()&&u.t(1)?
-				u.in('get')
-				:alert('fill name, password')
-			:chk.checked&&u.t()&&u.t(1)&&v==u.i[2].value?
-				u.in('add')
-				:alert(chk.msg)},
-	//extra fns
-	chk:function(){//class
-		lnk.className=lnk.className?(
+        u.ss(u.i[0].value),
+        //i.c('Logged In as: '+i.key),
+        u.i[0].value=u.i[1].value=u.i[2].value='',
+        u.shift())}
+    i.tr(opt)},
+  match:function(){return u.i[1].value==u.i[2].value},
+  vl:function(){var n=i.key=u.i[0].value,v=i.val=u.i[1].value,v=Number(v)||v//validate
+    u.lg?
+      u.t()&&u.t(1)?
+        u.in('get')
+        :alert(u.fill)
+      :chk.checked&&u.t()&&u.t(1)&&u.match()?
+        u.in('add')
+        :alert(chk.msg)},
+  //extra fns
+  chk:function(){//class
+    lnk.className=lnk.className?(
       chk.checked=!0,
-			chk.innerHTML='☑',
-			'')
-			:(
+      chk.innerHTML='☑',
+      '')
+      :(
       chk.checked=!1,
       chk.innerHTML='⬛',
-			'noread')},
-	cl:function(){//class
-		u.p.className=u.p.className=='no'?'':'no'
-		u.lg=!u.lg
-		fs.className=(lg.className=u.lg?'lg':'su')+'bg'},
-	sgn:function(){//sign up
-		chk.onclick!=u.chk&&(
-			chk.onclick=u.lp.onclick=u.chk,
-			chk.onkeypress=chk.click)},
-	cas:function(s,i){//create&append script
-    s=s.split(' ')
+      'noread')},
+  sgn:function(){//sign up
+    chk.onclick!=u.chk&&(
+    chk.onclick=u.sp.onclick=u.chk)},
+  cas:function(f){//create&append script
+    u.ich[1].classList.add('back')
+    u.a=0
+    var i,s='idb filler'.split(' ')
     for(i in s){
-  		fl=u.c('script')
-  		fl.src='js/'+s[i]+'.js'
-  		u.d.head.appendChild(fl)}},
-	init:function(){//initialize or initiate
-    lg.innerHTML='&nbsp;📝 🏠 '
-    var p,a,l=0,x=[
-      'jquery.com_jQuery v2.1.1',
-      'threejs.org_Three.js rv61, Ricardo Cabello. Barcelona, Spain',
-      'gildas-lormeau.github.io/zip.js_zip.js, Gildas Lormeau. Rennes, France',
-      'www.blender.org_blender 2.72a']
-		u.out=u.ss
-		u.i=u.g('input')
-		u.name=u.i[0],u.value=u.i[1]
-		lg.onclick=u.cl
-		u.sgn()
-		ok.onclick=u.vl
-		info.onclick=function(){open(location.href+'support','_blank')}
-		u.lg&&(u.cl(u.lg=0))
-		ch.msg=u.lng=='es'?(
-			u.tx(u.lp),
-      chk.msg='Por Favor verifique que usted:\n1: NO use carácteres especiales\n2: Llene el nombre minimo 3\n3: Llene y coincida la contraseña minimo 4\n4: Acepte los T&Cs',
-      'Si acepta, la pagina cargaria un 50% MAS RAPIDO\ny ademas tendria acceso a'+u.K+' AUN SIN internet!')
-			:(
-      chk.msg='Please check that you:\n1: DO NOT use special characters\n2: Fill name at least 3\n3: Fill and match password at least 4\n4: Accepted T&Cs',
-      'If you accept, this page would load 50% faster\nand furthermore you would have access to'+u.K+' EVEN WITHOUT internet!')
-    while(l<4){
-      a=u.c('a')
-      a.href='//'+(p=x[l++].split('_'))[0]
-      a.title=p[1]
-      a.target='_blank'
-      ft.appendChild(a)}}}
-u.init(u.lg=0)
-chk.checked=0
-onload=function(){
-  u.cas('idb filler')}
+      fl=u.c('script')
+      fl.src='js/'+s[i]+'.js'
+      u.d.head.appendChild(fl)}},
+  render:function(){
+    if(o&&){
+      !t.s.children.length-1&&o.ntH(0,o.asset)
+      !u.a&&(u.a=1,h.R())
+      cv.style.height=cv.style.width=''
+      cnt.className=''
+      !title.innerHTML&&(title.innerHTML='🏠 house ')
+      back.onclick=function(){
+        cv.className='out back'
+        cnt.className='no'
+        u.a=0
+        info.className='z'}}},
+  init:function(){var i//initialize or initiate
+    u.hch[0].className='active'
+    u.ich[0].onclick=function(){
+      this.className=cv.className=''
+      u.ich[1]!=cv&&intro.replaceChild(cv,u.ich[1])
+      u.render()}
+    for(i in u.hch)u.hch[i].idx=i,u.hch[i].onclick=u.shCl
+    prev.lim='one',  prev.l=-1
+    next.lim='three',next.l=1
+    prev.onclick=next.onclick=function(){
+      //console.log(this.lim,this.l),
+      u.shCl(u.cl.indexOf(roll.className)+this.l,this.lim)}
+    u.fch=[fs.children[0],fs.children[1]]
+    for(i in u.fch)u.fch[i].idx=i,u.fch[i].onclick=function(){
+      !this.className&&(
+        //console.log('shifting class'),
+        u.gc(this,'lgsu'),
+        u.shcl())}
+    u.out=u.ss
+    u.i=u.g('input')
+    u.name=u.i[0],u.value=u.i[1]
+    u.sgn()//assigning check-event ☑⬛
+    ok.onclick=u.vl
+    for(i in u.i)if(!isNaN(i))u.i[i].idx=Number(i),u.i[i].onkeypress=function(){th=this
+      setTimeout(function(){
+        if(!u.lg)th.className=th.idx?
+          (u.i[2].value&&(u.i[th.idx>1?1:2].className=u.match()))&&u.t(th.idx)
+          :u.t(th.idx)})}
+    ch.msg=(u.lng=/(es||en)/.exec(u.nv.language)[0])=='es'?(
+      u.tx(u.sp),
+      u.lgErr='Usuario o Contraseña incorrecta',
+      u.suErr='Este usuario ya existe',
+      u.fill='Por Favor verifique que usted:\n1: NO use carácteres especiales\n2: llene el nombre minimo 3\n3: llene y coincida la contraseña minimo 4',
+      chk.msg=u.fill+'\n4: Acepte ☑ los T&Cs',
+      'Si acepta, la pagina cargaria un 50% MAS RAPIDO\ny ademas tendria acceso a esta pagina AUN SIN internet!')
+    :(
+      u.lgErr='Incorrect Username or Password',
+      u.suErr='This username already exists',
+      u.fill='Please check that you:\n1: DO NOT use special characters\n2: Fill name at least 3\n3: Fill and match password at least 4',
+      chk.msg=u.fill+'\n4: Accepted T&Cs',
+      'If you accept, this page would load 50% faster\nand furthermore you would have access to this website EVEN WITHOUT internet!')}}
+console.clear()
+u.init()
+onload=u.cas
