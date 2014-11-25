a9a13fbd0664f97c9eb21a16ec57ebf1c47de157
fernandoavila <luisfernandoavila@outlook.com>
2014-11-25 08:17:12 +0000

    api button
    api button shifts between wuxing and man

--- api.dv.js
+++ api.dv.js
@@ -11,14 +11,14 @@ a={//API
       console.log(a.d.readyState),
       onload=a.init)},
   sty:function(shi){//set: division,unique id
-    var id=(shi='.'+shi).slice(2),sty=a.new('style'),kj=a.cn,bt=shi+'>button',cv=shi+'>canvas'
+    var id=(shi='.'+shi).slice(2),sty=a.new('style'),kj=a.kj,bt=shi+'>button',cv=shi+'>canvas'
     sty.innerHTML=
-      shi+'{background:#180;height:100%;width:100%;z-index:999999}\n'+
-      bt+'{background:#015;border-radius:1em}\n'+
+      shi+'{background:#180;z-index:999999}\n'+
+      bt+'{background:#015;border-radius:5px}\n'+
       cv+'{background:#057E9C}\n'+
       shi+','+bt+'{color:#FFF;display:table;font-family:Segoe UI Symbol;font-size:2em;position:fixed}\n'+
       '.'+kj.man+id+','+cv+'{height:100%;width:100%}\n'+
-      kj.wuxing+id+'{display:none}'
+      '.'+kj.wuxing+id+'{display:none}'
     return sty},
   url:function(b){//url from blob protocol
     return URL.createObjectURL(new Blob(['']))},
@@ -32,7 +32,7 @@ a={//API
     无形 Wúxíng: invisible
     满 Mǎn: full*/
     v=p.innerHTML//ok, gathering result
-    a.cn={
+    a.kj={
       shi:v[0],
       wuxing:v[1]+v[2],
       man:v[3]}
@@ -41,8 +41,13 @@ a={//API
     a.a(p,'body')//append button&canvas to div
     a.a(b,p)
     a.a(c,p)
-    c.className=a.cn.wuxing+u
+    c.className=a.kj.wuxing+u
     a.a(a.sty(p.className=v[0]+u),'head')//appending 绝 后 style to head
     a.bt.onclick=function(){
-      console.log('end')}}}
+      a.cv.className=a.cv.className?(
+        a.p.classList.add(a.kj.man+a.uid),
+        '')
+        :(
+        a.p.classList.remove(a.p.classList[1]),
+        a.kj.wuxing+a.uid)}}}
 a.set()
