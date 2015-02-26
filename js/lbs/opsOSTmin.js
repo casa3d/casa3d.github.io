01b350d912871302e28278fbfbe25233da62f32a
brackenavila <luisfernandoavila@outlook.com>
2015-02-25 19:12:17 -0500

    js lbs

---
+++ js/lbs/opsOSTmin.js
@@ -0,0 +1,384 @@
+o={
+  a:u.new('a'),
+  ap:'application/',
+  asset:asset,
+  Cs:[
+    'xyzxyz'.split(''),
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0]],
+  ελ:'αβγδεζηθι'.split(''),
+  hs:{
+    ch:[],
+    cp:u.new('input',{onchange:u.f("u.sty('.c{background:'+(u.pc=this.value)+';src:0'),u.d.cookie='c3d.c='+u.pc"),type:/Trident/.test(h.nv.userAgent)?'':'color',value:u.pc}),
+    fn:[
+      u.f('o.hs.cp.click()'),
+      u.f('o.this=this,o.iz.click()'),
+      u.f('o.exp()'),
+      u.f("open(location.href+'support','_new')"),
+      u.f('u.shift(setTimeout(u.out,100))')],
+    slot:u.f('n,s',"u.ap(u.new('span',{innerHTML:o.mez(s)+' | '+n}),师二.children[0])"),
+    tt:t.eval(
+      'Selecciona el Color Preferido_'+
+      'Añade una Casa zip\n               Z_'+
+      'Guarda la Casa que esta al frente de la Cámara\n                                   G_'+
+      'Abre la Página de Información\n                      I_'+
+      'Bloquea la Sesión\n             B_')},
+  img:{
+    _id:u.s('ho br do fl gl me wa wi wo'),
+    en:u.s('user brick door floor glass metal wall window wood'),
+    es:u.s('usuario ladrillo puerta piso vidrio metal pared ventana madera'),
+    add:function(){
+      o.add(o.mn.up?
+        this.src
+        :this.src.split(/.*\/(.*)\./)[1])}},
+  mt:new T.MeshBasicMaterial,
+  ob3:new T.Object3D,
+  S:510,
+  Π:Math.PI,
+  π:Math.PI/2,
+  pl:u.f('a,b','return new T.PlaneGeometry(a,b)'),
+  URL:u.f('b','return URL.createObjectURL(b)'),
+  ae:u.f('e,a','for(var i in a)e.addEventListener(i,a[i])'),
+  tn:{
+    blob:[]},
+  tdp:function(h,s){
+    return{
+      curveSegments:1,
+      font:'janda manatee solid',
+      height:h,
+      size:s}},
+  lr:function(c,r){
+    var s=c.style,p=Number(s.left.slice(0,-1))
+    this.r||r?
+      Math.abs(p/100)+1<c.childElementCount/10&&(s.left=p-100+'%')
+      :p&&(s.left=p+100+'%')},
+  m:[[1e6,'MB'],[1e3,'KB'],[1,'bytes']],
+  mez:function(n){var i,d//number
+    for(i in o.m)if(n>(d=o.m[i][0]))break
+    return Number((n/d).toFixed(2))+' '+o.m[i][1]},
+  e:u.s('⥀ 👁 ⥁   📹'),
+  op:[
+    u.f('m.r(m.ρκ=0)'),
+    m.act,
+    u.f('m.rt(2)'),
+    dr.act,
+    function(i){
+      i=u.d[this.i]
+      u[i?'d':'b'][i?this.c:this.r]()},
+    function(){var p=m.P,r=m.c,v=o.Cs,i,fC,sC,α,ε
+      this.innerHTML=this.innerHTML=='🎥'?(
+        fC=1,
+        sC=2,
+        '📹')
+        :(fC=2,
+        sC=1,
+        '🎥')
+      for(i in v[0]){
+        ε=i<3?p:r,α=v[0][i]
+        v[fC][i]=ε[α]
+        ε[α]=v[sC][i]}},
+    u.f('o.rst(0)'),
+    u.f('o.rst(1)')],
+  vlz:function(v){
+    (v=this.value)?
+      m.i=v
+      :this.value=m.i},
+  zhCL:'左上权'.split(''),
+  in:function(a,f,m){
+    return u.new(
+      'input',
+      {type:'file',
+      accept:a,
+      multiple:m,
+      onchange:u.f(f+'(this.files)')})},
+  rst:function(o){
+    var dc=地点.children
+    dc[1].children[o].value=dc[2].children[o].value=dc[3].children[o].value=0
+    c[o?'rotation':'position'].set(0,0,0)},
+  mn:function(b){
+    var i=0,D,s=u.s('🌄  ⤴ ⤿ 🔘 ⤾ ⤵'),e
+    u.ap(D=o.mn=u.new('div',{id:'opsMenu',className:'no'}),h.tls)
+    D.rt=u.f('xy,p','o.mn.o&&(o.mn.o.rotation[xy]=o.mn.o.rotation[xy]+o.π/2*p)')
+    D.ф=u.f('s','o.mn.o.material.map=T.ImageUtils.loadTexture(s)')
+    D.fn=[
+      u.f('o.i.click(),o.mn.up=1'),
+      function(O){
+        u.sc(D,'no'),
+        (O=o.mn.o)&&(
+          O.parent.id==o.added.id?
+            o.added.remove(O)
+            :t.s.children[1].remove(O),
+          o.mn.up=0)},
+        'x',
+        'y',
+        u.f('o.mn.o&&o.mn.o.rotation.set(0,0,0)'),
+        'y',
+        'x']
+    for(i in s){
+      u.ap(e=u.new(b,{innerHTML:s[i]}),D)
+      e.onclick=i<2||i==4?
+        D.fn[i]
+        :u.f('o.mn.rt("'+D.fn[i]+'",'+(i<5?'-':'')+'1)')
+      i++}},
+  usrAgnt:{Chrome:'webkit',Trident:'ms',Firefox:'moz',f:'FullScreen'},
+  rb:function(F){
+    if(F.length){
+      var x,i=0,I,a,ф,b=o.tn.blob,de
+      x=setInterval(function(){de=1,f=F[i]
+        for(I in b)if(b[I].sz==f.size&&!(de=0))break
+        o.add(
+          de?(
+            ф=o.URL(f),
+            b.push(a={nm:f.name,src:ф,sz:f.size}),
+            ф)
+            :b[I].src,
+          a,
+          i)
+        if(++i>=F.length)clearInterval(x),de&&o.lr(图片,1)},100)
+      o.F=F}
+    else o.mn.up=0},
+  add:function(src,a,i){
+    var cp=c.position,P=((cp.x+o.S/2)/o.S+1+'')[0],p=o.ob3.clone()
+    a?(
+      u.ap(
+        u.new('div',{
+          innerHTML:'<img src='+src+'><label>'+a.nm+'<br>'+o.mez(a.sz),
+          onclick:u.f('o.add(this.children[0].src)')}),
+        图片),
+      !i&&(
+        o.mn.up?
+          o.mn.ф(src)
+        :o.add(src)))
+      :o.mn.up?
+        o.mn.ф(src)
+        :(
+        t.ad({
+        a:'10 20['+src+'] ',
+        p:P>0?
+          t.s.children[P]||p
+          :p,
+        gp:t.s}),
+        setTimeout(function(v,i,g,o){
+          v=(g=(o=t.shape).geometry).vertices
+          v[0].x=v[0].y=10,v[2].y=(v[1].x=90)+100
+          dr.rsz.centre(t.shape)
+          o=o.position
+          o.x=cp.x
+          o.y=cp.y
+          o.z=cp.z-200
+          cp.y+=.1
+          cp.z+=.1},10))},
+  cc:function(){
+    var c=t.s.__lights[0].color
+    c.r=c.g=c.b=this.value/100},
+  ld:u.f('e','alert(e||"100% ?")'),
+  dwn:function(n){
+    o.a.download='casa#'+n+' ('+new Date().toUTCString().split(/, | GMT/)[1]+').zip'
+    if(h.nv.msSaveBlob)h.nv.msSaveBlob(blb,o.a.download)
+    else{
+      clickEvent=u.d.createEvent('MouseEvent')
+      clickEvent.initMouseEvent('click',1,1,h.w,0,0,0,0,0,0,0,0,0,0,null)//type, canBubble, cancelable, view,  detail, screenX, screenY, clientX, clientY,  ctrlKey, altKey, shiftKey, metaKey,  button, relatedTarget
+      o.a.dispatchEvent(clickEvent)}},
+  str:function(P){
+    var x,g,s='',m,M,f,r,p,rt,i,v
+    x=t.s.children[1].children[P]
+    p=x.position
+    r=x.rotation
+    if(g=x.geometry)s=g.width/100+' '+g.height/100
+    else{s=''
+      i=(x=x.children).length
+      v=(x=x[0]).geometry
+      if(i>1){
+        s+='B'+v.width/100+' '+v.height/100+' '+v.depth/100+' x'+i}
+      else{
+        v=v.vertices
+        for(i in v)s+=Number((v[i].x/100).toFixed(6))+','+Number((v[i].y/100).toFixed(6))+' '
+        s=s.slice(0,-1)}}
+    m=(M=x.material).map
+    f=/img\/(.*)\./.exec(m.sourceFile)[1]
+    rp=(rp=m.repeat.x)!=1?' '+rp:''
+    s+='['+f+rp+']'
+    function d(l,a){
+      if(l||a)l=(a==1?'':' ')+Number((l/100).toFixed(6))
+      return l||''}
+    p=(p.x||p.y)?d(p.x,1)+d(p.y,2)+d(p.z):''
+    s+=p
+    function rt(l,y){// αχζισ аксыс
+      if(l){var i=l<0?'-':''
+        l=' '+i+(/1.57/.test(l)?
+          'p'
+          :'P')+(y||'')}
+      return l||''}
+    χ=rt(r.x)//rotation x
+    γ=rt(r.y,'y')//rotation y
+    s+=χ+γ+(M.side==2?'':'.')+'_'
+    rz+=s},
+  zip:function(){
+    return zip&&zip.method&&zip.method.deflate},//compress
+  exp:function(){//export option: house as js inside zip
+    if(o.zip()){
+      if(t.s.children.length-1){
+        var p=((c.position.x+o.S/2)/o.S+1+'')[0],s,i//position,scene,iterator
+        u.c(p=p>0?p:1)
+        /*cpx=camera.position.x, o.S=510
+        why offset=cpx+255?
+        [ . ] I'd say such "dot" is .5 or 50% but actually it's... 0 "just like vertices work" center
+              but I wanted it to be like [   ] "dot starts in left square-bracket"
+              so, I set as offset its half which actually is 255 "see: o.S"
+        Y offset/510?
+            'coz I wanted it to set every house apart referenced by cpx
+        Y you added 1 to last result?
+          'coz I have no house starting at 0 position and also 'coz I cannot let user see h0, h1, h2 "it feels freaky, doesn't it?"
+        Y u ended up enclosing it in Quotation marks?
+          strings may be treated as vector whilst number not
+          that way I'll get very first number #
+          and therefore user'd do two things:
+            1) now in which current house he/she is
+            2) export file like so: casa# dateInfo.zip */
+        //o.str(p)//spawn string providing its position
+        rz='',s=t.s.children
+        s=s[p]||s[s.length-1]
+        for(i in s.children)o.str(i)
+        s=o.hs.ch[p-1]//final string
+        x=new Blob([rz.slice(0,-1)]/*[#-1] 'cuz it'd never be 0*/,{type:o.ap+'JavaScript'})//house,mimeType
+        x.name='casa3d.js'
+        zip.createWriter(
+          new zip.BlobWriter(o.ap+'zip'),
+          function(e){
+            zipWriter=e
+            zipWriter.add(
+              x.name,
+              new zip.BlobReader(x),
+              function(){
+                //u.c('success reading blob')
+                o.a.hidden=0
+                zipWriter.close(function(e){
+                  o.a.href=o.URL(blb=e)
+                  o.dwn(p)})})})}//since 0-->1, this way I'll now current house
+      else 0}//in order to download house, first add one or upload your own.
+    else o.ld()},
+  imp:function(s,f){//string, file
+                                //510
+    var p,n,l=h.ntH.children.length,S=o.S*l//space between each house h.ntH (its container) has got -40 in x axis, though
+    t.ad({a:t.eval(s),p:p=o.ob3.clone(),gp:t.s})
+    c.position.set(p.position.x=S,1e3,1700)//1700 so that camera can display almost all houses&numbers
+    c.rotation.set(0,0,0)//make me sure that camera is infront of them
+    h.ntH.add(n=new T.Mesh(new T.TextGeometry(l+1,o.tdp(1,100)),o.mt))//number
+    n.position.x=S-(l>8?30:0)
+    f&&(//if it comes from file reader then it means it's new
+      o.hs.ch.push({raw:s,name:f.name,size:f.size}),
+      o.hs.slot(f.name,f.size))},
+  ext:function(F){//extract house from zip
+    if(F&&F[0]&&o.zip()){var i,c=o.hs.ch,z=F[0]//zipped file
+      for(i in c)if(z.size==c[i].size)return
+      zip.createReader(
+        new zip.BlobReader(z),
+        function(zr){//on read end
+          zr.getEntries(function(e){//entries
+            if(e=e[0])e.getData(
+              new zip.BlobWriter(),
+              function(blob){
+                fr=new FileReader()
+                fr.onload=function(){
+                  o.imp(
+                    this.result,
+                    z)}
+                fr.readAsText(blob)},null)})})}
+      else o.ld()},
+  o:{id:''},
+  init:function(){console.clear()
+    var l,op,p,i,w,n,U,d='div',D,b='button',
+    f=function(T,s,i,t,l,S){s=s.split('.'),S='';for(l in s)S+='<'+T+(i?' id='+i+(t?' title='+t:''):'')+'>'+s[l]+'</'+T+'>';return S},
+    B=function(s,i,t){return f(b,s,i,t)},
+    D=function(s,i,t){return f(d,s,i,t)}
+    a=function(t,a,p,c){//tag, attrs, parent
+      u.ap(u.new(t,p?a:0),p?p:a)}
+
+    h.ga.add(o.added=o.ob3.clone())
+    o.added.add(o.grH=new T.GridHelper(1e4,40))
+    o.grH.position.y=-.9
+
+    h.tls.innerHTML+=D(B('➕')+B('«')+D(D('','图片'))+B('»'),'th')
+    a(b,{id:'tg',innerHTML:'🌄'},th)
+    a(d,{id:'mn',innerHTML:D('🔅<input id=lit type=range>')+D('')},h.tls)
+    op=mn.children[1]
+    for(l in o.e)a(b,{innerHTML:o.e[l]},op)
+    a('input',{type:'number'},op)
+
+    //地点: location (Dìdiǎn)
+    l=D('<input><input>')
+    u.ap(u.new('div',{id:'地点',innerHTML:D(B('')+B('🔘'))+l+l+l}),mn)
+
+    h.tls.innerHTML+=D(B('▩','tg')+D('','师一')+D('','师二'),'大师')
+    lit.value=100
+    o.mn(b)//contextMenu
+    x=[师一,师二,th,mn,opsMenu]
+    for(i in x)u.tc(x[i],'c')
+    //u.c('p:'+p+', w:'+w)
+    p=(U=o.usrAgnt)[/(Chrome|Trident|Firefox)/.exec(h.nv.userAgent)[0]],w=p=='webkit',n=w?16:p=='ms'?15:20
+    u.sty('#cnt>*+*{height:'+(w?76:85)+'%}#mn{font-size:1.5em;top:11%;width:'+n+'%;background-image:linear-gradient(90deg,rgba(0,0,0,.3),rgba(255,255,255,.3)93%,rgba(0,0,0,.6))}#th,#师二{background-image:linear-gradient(rgba(255,255,255,.2),rgba(0,0,0,.5)90%,rgba(255,255,255,.5))}#lit{width:60%}#edt '+b+':not(#tg){color:rgba(255,255,255,.8)}'+(w?'':'#mn button:not(#tg){margin:-.11em}')+'#mn>:first-child,#地点>*~*{color:#FD0}#mn #tg{left:100%;top:39%}input[type=number]{margin-left:2em;top:-.8em;width:3.5em}#地点>:nth-child(1)>*,#opsMenu>:nth-child(-n+2){box-shadow:#000 0 -2px 10px;width:50%}#地点>:nth-child(2):before{content:"X "}#地点>:nth-child(3):before{content:"Y "}#地点>:nth-child(4):before{content:"Z "}#地点 input{width:40%'+(w?';font-size:1.25em':'')+'}#大师{height:'+(w?77:88)+'%;right:0;width:40%}#tg{color:rgba(0,0,0,.2);padding:0}#大师>#tg{top:45%}#师一{width:25%}#师二{width:70%}#师二>*{height:'+100/9+'%;width:100%}#师一>*,#师二>*>*{border:5px solid transparent}#师一>*{height:9.5%;width:91.5%}#师一>*>*{width:100%}#师一 label{display:inline-block;top:-'+(w?3:4)+'5%}#师二{top:0;height:900%}#师二>*~*>*{height:14.15%;width:21.63%}#师二>*~*>:first-child{height:16.5%;width:96.2%}#师二>*~*>:nth-child(n+2):nth-child(-n+3){height:29.76%}#师二>*~*>:nth-child(10){height:14.15%;width:46.2%}#师二>*~*>:nth-child(11){height:29.84%;width:46.2%}#th{top:0;height:11%}#th>div{width:85%}#th>button:not(#tg),#大师>#tg{width:5%;z-index:1}#th>#tg{left:48.5%;top:-15%}#师二>:first-child>*{width:95%}#师二 button{width:20%}#师二 input{border:0;height:0;opacity:0;width:0}#师二>:first-child>:nth-child(odd){background:rgba(0,0,0,.1)')
+    setTimeout(function(){var i,l,x,I=o.img,lng=I[u.lng=='es'?'es':'en'],I=I._id,g=function(e,t){return e.getElementsByTagName(t)}
+      u.a=!u.a
+      for(l in I){ι=I[l],L=0
+        u.sty('.'+o.ελ[l]+'{top:-'+l+'00%')
+        a('div',{innerHTML:(l==0?B(''):'<img src=img/'+ι+'0.jpg>')+'<label>'+lng[l],onclick:u.f('u.gc(this,"中"),师二.className="'+o.ελ[l]+'"')},师一)
+        u.ap(x=u.new('div'),师二)
+        if(l==0)x.innerHTML=D(B('🎨..💾.ⓘ.🔒')),u.ap(o.hs.cp,x.firstChild),u.tc(x,'c')
+        else while(L<15)a('img',{src:'img/'+ι+(L++)+'.jpg',onclick:o.img.add},x)}
+      u.sc(师一.firstChild,'中')
+
+      lit.onchange=o.cc
+      bs=g(mn,b)//buttons
+      for(l in o.op)bs[l].onclick=o.op[l]
+      a(b,{innerHTML:'🏃',id:'tg'},mn)
+      u.at(o.vl=bs[5].nextSibling,{value:1,step:.25,onchange:o.vlz})
+      D=g(地点,'input')
+      m[0]=D[0],m[1]=D[2],m[2]=D[4]//position xyz
+      m[3]=D[1],m[4]=D[3],m[5]=D[5]//rotation xyz
+
+      u.sty('#edt button:not(#tg):hover{background:rgba(255,255,255,.2);text-shadow:#000 -15px 10px 7px;color:#FFF}#opsMenu{background:radial-gradient(rgba(12,175,124,.5),rgba(24,55,74,.8));border-radius:5px;left:0;top:0;width:10%}#opsMenu>:nth-child(3),#opsMenu>:last-child{width:100%}#opsMenu>button:nth-child(n+4):nth-child(-n+6){width:33%}#图片>:nth-child(even) *+*,#师一 label{background:rgba(0,0,0,.5)}#师二 img:active,.中{-'+p+'-transform:rotateY(5deg)}#图片{width:1000%}#图片>*{width:1%}#th label{top:-30%;display:inline-block}#th label:hover{top:-104%}#fs,canvas,'+b+',#edt{-'+p+'-user-select:none}#图片>:nth-child(odd) *+*{background:rgba(255,255,255,.7);text-shadow:#000 0 0 0;color:#000}#师一>*:hover,#师二>*>*:hover{border:5px solid rgba(0,0,0,.5)}#mn,#opsMenu *{left:0;padding:0}.上{top:-11%}.左{left:-'+n+'%}.权{right:-38%}.看{text-shadow:cyan -5px 5px 5px')
+
+      i=0,x=g(th,b),tg=[mn.lastChild,th.lastChild,大师.firstChild]
+      while(i<x.length-1)x[i].onclick=u.f(i?'o.lr(图片,'+(i-1)+')':'o.i.click()'),tg[i].onclick=u.f('u.tc(this.parentNode,"'+o.zhCL[i++]+'")')
+      o.i=o.in('image/*','o.rb',1)
+      o.iz=o.in(o.ap+'zip','o.ext')//extract zip
+      b2=g(师二,'button')
+      for(i in o.hs.fn)u.at(b2[i],{onclick:o.hs.fn[i],title:o.hs.tt[i]})
+      //u.sc(th,'上'),u.sc(mn,'左'),u.sc(大师,'权')
+      o.yan=bs[1]
+
+      u.at(U,
+        {is:(b=bs[4]).i=p+(p=='webkit'?'Is':'')+U.f,
+        ca:b.c=p+'Cancel'+U.f,
+        rq:b.r=p+'Request'+U.f})
+      o.x={49:tg[0],50:tg[1],51:tg[2],98:b2[4],99:bs[5],103:b2[2],105:b2[3],122:b2[1]}
+      o.dv=[mn,th,大师]
+      o.ae(u.d,
+        {contextmenu:function(e){
+          if(!edt.className){var r,s,c='no'
+            e.preventDefault(),e.stopPropagation()
+            /*!dr.app&&dr.act()*/
+            r=dr.rc(e,dr.xy(e))
+            if(r&&(o.mn.o=r=r.object)&&r.id!=6){s=o.mn.style,c=''
+              s.left=e.clientX+'px'
+              s.top=e.clientY+'px'}
+            o.mn.up=!u.sc(o.mn,c)}},
+        keydown:function(e){//escapeKey toggle event
+          if(e.which==52){var i,x=o.zhCL
+            //u.c(e)
+            for(i in x)u.tc(o.dv[i],x[i])}},
+        keypress:function(e){
+          if(!edt.className){var k=e.which//or charCode but keyCode thx Firefox
+            o.x[k]&&o.x[k].click()}}})
+      setTimeout(function(){var s=
+        t.eval('Gira un ¼ de vuelta hacia la izquierda\n                           ⇧+_'+
+          'Activa/Desactiva la Exploración\n                      esc_'+
+          'Gira un ¼ de vuelta hacia la Derecha\n                          ⇧+➔_'+
+          'Activa/Desactiva la Edición de los Objetos\n                               M_'+
+          'Alterna el Modo de Pantalla\n                 fn+F11_'+
+          'Alterna entre Cámaras A B\n                   C_'+
+          'Reinicia todos los Ejes de la Posición_'+
+          'Reinicia todos los Ejes de la Rotación')
+        for(l in s)bs[l].title=s[l]
+        o.vl.title='Ajusta la Velocidad de la Cámara'},1e3)},1e3)}}
+o.init()
